36d35b7c79c97bb31b710579dcbdb5a474512e4a
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-05-08 14:38:19 +0500

    Just apply media Query on gridview.builder

--- mediaquery/lib/mediaQueryView.dart
+++ mediaquery/lib/mediaQueryView.dart
@@ -6,14 +6,26 @@ class mediaQuery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello Media Query"),
-      ),
-      body: Container(
-        width: MediaQuery.of(context).size.width * 0.2,
-        height: MediaQuery.of(context).size.height * 0.5,
-        color: Colors.amber,
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Hello Media Query"),
+        ),
+        body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              childAspectRatio: MediaQuery.of(context).size.height * 0.002),
+          itemCount: 20,
+          itemBuilder: (context, Index) {
+            return Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(07),
+                color: Colors.amber[100],
+              ),
+
+              // height: MediaQuery.of(context).size.height * 0.3,
+              // width: MediaQuery.of(context).size.width * 0.6,
+            );
+          },
+        ));
   }
 }
